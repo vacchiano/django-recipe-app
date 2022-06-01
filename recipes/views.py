46645c241d0ab103776fda69257282bc7ed8b776
Dom Vacchiano <djvacchiano@gmail.com>
2022-06-01 13:36:11 -0500

    updated dummy data

--- recipes/views.py
+++ recipes/views.py
@@ -4,19 +4,19 @@ recipes = [
   {
     'author': 'Dom V.',
     'title': 'Meatballs',
-    'content': 'Combine ingredients, form into balls, brown, then place in oven.',
+    'description': 'Combine ingredients, form into balls, brown, then place in oven.',
     'date_posted': 'May 18th, 2022'
   },
   {
     'author': 'Gina R.',
     'title': 'Chicken Cutlets',
-    'content': 'Bread chicken, cook on each side for 8 min',
+    'description': 'Bread chicken, cook on each side for 8 min',
     'date_posted': 'May 18th, 2022'
   },
   {
     'author': 'Bella O.',
     'title': 'Sub',
-    'content': 'Combine ingredients.',
+    'description': 'Combine ingredients.',
     'date_posted': 'May 18th, 2022'
   }
 ]
